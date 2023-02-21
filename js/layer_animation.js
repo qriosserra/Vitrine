60e6544981fd308be7ba6e53c04fa5f35db5cc34
qriosserra <q.riosserra@gmail.com>
2023-02-21 21:18:01 +0100

    first commit

---
+++ js/layer_animation.js
@@ -0,0 +1,55 @@
+const layer0 = KUTE.fromTo(
+    "#layer0_0",
+    {path: "#layer0_0"},
+    {path: "#layer0_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+layer0.start();
+
+const layer1 = KUTE.fromTo(
+    "#layer1_0",
+    {path: "#layer1_0"},
+    {path: "#layer1_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+layer1.start();
+
+const layer2 = KUTE.fromTo(
+    "#layer2_0",
+    {path: "#layer2_0"},
+    {path: "#layer2_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+layer2.start();
+
+const layer3 = KUTE.fromTo(
+    "#layer3_0",
+    {path: "#layer3_0"},
+    {path: "#layer3_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+layer3.start();
+
+const layer4 = KUTE.fromTo(
+    "#layer4_0",
+    {path: "#layer4_0"},
+    {path: "#layer4_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+layer4.start();
+
+const background0 = KUTE.fromTo(
+    "#background0_0",
+    {path: "#background0_0"},
+    {path: "#background0_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+background0.start();
+
+const background1 = KUTE.fromTo(
+    "#background1_0",
+    {path: "#background1_0"},
+    {path: "#background1_1"},
+    {repeat: 999, duration: 10000, yoyo: true}
+)
+background1.start();
